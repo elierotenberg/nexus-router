3d1aabff16394efc829ca486fd5a327088803e24
Elie Rotenberg <elie@rotenberg.io>
2014-12-27 20:15:29 +0100

    Upgraded all deps.

--- dist/ConstantRouter.js
+++ dist/ConstantRouter.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var _extends = function (child, parent) {
-  child.prototype = Object.create(parent.prototype, {
+var _inherits = function (child, parent) {
+  child.prototype = Object.create(parent && parent.prototype, {
     constructor: {
       value: child,
       enumerable: false,
@@ -9,33 +9,29 @@ var _extends = function (child, parent) {
       configurable: true
     }
   });
-  child.__proto__ = parent;
+  if (parent) child.__proto__ = parent;
 };
 
-require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;module.exports = function (Router) {
+require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = process.env.NODE_ENV !== "production";var __PROD__ = !__DEV__;var __BROWSER__ = typeof window === "object";var __NODE__ = !__BROWSER__;module.exports = function (Router) {
   var _ = require("lodash-next");
 
-  var ConstantRouter = (function (Router) {
-    var ConstantRouter =
-    // Simplified Router which maps pattern to flat values.
-    function ConstantRouter(map) {
-      _.dev(function () {
-        return map.should.be.an.Object && Object.keys(map).map(function (pattern) {
-          return pattern.should.be.a.String;
-        });
+  var ConstantRouter =
+  // Simplified Router which maps pattern to flat values.
+  function ConstantRouter(map) {
+    _.dev(function () {
+      return map.should.be.an.Object && Object.keys(map).map(function (pattern) {
+        return pattern.should.be.a.String;
       });
-      var routes = _.mapValues(map, function (value) {
-        return function () {
-          return value;
-        };
-      });
-      Router.call(this, routes);
-    };
+    });
+    var routes = _.mapValues(map, function (value) {
+      return function () {
+        return value;
+      };
+    });
+    Router.call(this, routes);
+  };
 
-    _extends(ConstantRouter, Router);
-
-    return ConstantRouter;
-  })(Router);
+  _inherits(ConstantRouter, Router);
 
   return ConstantRouter;
 };
--- dist/Router.js
+++ dist/Router.js
@@ -4,7 +4,7 @@ var _toArray = function (arr) {
   return Array.isArray(arr) ? arr : Array.from(arr);
 };
 
-require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;var _ = require("lodash-next");
+require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = process.env.NODE_ENV !== "production";var __PROD__ = !__DEV__;var __BROWSER__ = typeof window === "object";var __NODE__ = !__BROWSER__;var _ = require("lodash-next");
 
 var optionalParam = /\((.*?)\)/g;
 var namedParam = /(\(\?)?:\w+/g;
@@ -29,84 +29,78 @@ function extractFragmentParameters(regexp, fragment) {
   });
 }
 
-var Router = (function () {
-  var Router = function Router(routes) {
-    var _this = this;
-    if (routes === undefined) routes = {};
-    return (function () {
-      _.dev(function () {
-        return routes.should.be.an.Object && Object.keys(routes).map(function (pattern) {
-          return pattern.should.be.a.String && routes[pattern].should.be.a.Function;
-        });
-      });
-      _this._routes = {};
-      Object.keys(routes).forEach(function (pattern) {
-        return _this.route(pattern, routes[pattern]);
-      });
-    })();
-  };
+var Router = function Router(routes) {
+  var _this = this;
+  if (routes === undefined) routes = {};
+  _.dev(function () {
+    return routes.should.be.an.Object && Object.keys(routes).map(function (pattern) {
+      return pattern.should.be.a.String && routes[pattern].should.be.a.Function;
+    });
+  });
+  this._routes = {};
+  Object.keys(routes).forEach(function (pattern) {
+    return _this.route(pattern, routes[pattern]);
+  });
+};
 
-  Router.prototype.route = function (pattern, fn) {
-    var _this2 = this;
-    if (!fn) {
-      _.dev(function () {
-        return (_this2._routes[pattern] !== void 0).should.be.ok;
-      });
-      return this._routes[pattern];
-    }
+Router.prototype.route = function (pattern, fn) {
+  var _this2 = this;
+  if (!fn) {
     _.dev(function () {
-      return (_this2._routes[pattern] === void 0).should.be.ok && fn.should.be.a.Function;
+      return (_this2._routes[pattern] !== void 0).should.be.ok;
     });
-    if (pattern === null) {
-      return this["default"](fn);
-    }
-    var regexp = routeToRegExp(pattern);
-    this._routes[pattern] = { regexp: regexp, fn: fn };
-    return this;
-  };
+    return this._routes[pattern];
+  }
+  _.dev(function () {
+    return (_this2._routes[pattern] === void 0).should.be.ok && fn.should.be.a.Function;
+  });
+  if (pattern === null) {
+    return this["default"](fn);
+  }
+  var regexp = routeToRegExp(pattern);
+  this._routes[pattern] = { regexp: regexp, fn: fn };
+  return this;
+};
 
-  Router.prototype.routes = function (patterns) {
-    var _this3 = this;
-    if (!patterns) {
-      return this._routes;
-    }
-    Object.keys(patterns).forEach(function (pattern) {
-      return _this3.route(pattern, patterns[pattern]);
-    });
-    return this;
-  };
+Router.prototype.routes = function (patterns) {
+  var _this3 = this;
+  if (!patterns) {
+    return this._routes;
+  }
+  Object.keys(patterns).forEach(function (pattern) {
+    return _this3.route(pattern, patterns[pattern]);
+  });
+  return this;
+};
 
-  Router.prototype["default"] = function (fn) {
-    if (!fn) {
-      return this._default;
-    }
-    this._default = fn;
-    return this;
-  };
+Router.prototype["default"] = function (fn) {
+  if (!fn) {
+    return this._default;
+  }
+  this._default = fn;
+  return this;
+};
 
-  Router.prototype.match = function (fragment) {
-    var _this4 = this;
-    var res = null;
-    Object.keys(this._routes).forEach(function (pattern) {
-      var regexp = _this4._routes[pattern].regexp;
-      var fn = _this4._routes[pattern].fn;
-      if (res !== null) {
-        return;
-      }
-      if (fragment.match(regexp) !== null) {
-        var params = extractFragmentParameters(regexp, fragment);
-        params.push(fragment);
-        res = fn.apply(null, _toArray(params));
-      }
-    });
-    if (!res && this._default) {
-      res = this._default.call(null, fragment);
+Router.prototype.match = function (fragment) {
+  var _this4 = this;
+  var res = null;
+  Object.keys(this._routes).forEach(function (pattern) {
+    var regexp = _this4._routes[pattern].regexp;
+    var fn = _this4._routes[pattern].fn;
+    if (res !== null) {
+      return;
     }
-    return res;
-  };
-
-  return Router;
-})();
+    if (fragment.match(regexp) !== null) {
+      var params = extractFragmentParameters(regexp, fragment);
+      params.push(fragment);
+      res = fn.apply(null, _toArray(params));
+    }
+  });
+  if (!res && this._default) {
+    res = this._default.call(null, fragment);
+  }
+  return res;
+};
 
 var ConstantRouter = require("./ConstantRouter")(Router);
 
